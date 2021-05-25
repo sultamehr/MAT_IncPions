6a2144576729a45894d60b7fac10a2d0415d9945
Andrew Olivier <andrew@djoinc.com>
2021-05-25 16:46:12 -0500

    Updated best guess of what runTransWarp's histograms will be named.

--- runTransWarp.sh
+++ runTransWarp.sh
@@ -2,11 +2,12 @@
 
 #Usage: runTransWarp.sh runEventLoop.root warped.root
 
+VARIABLE=pTmu
 MIGRATION_FILE=$1
-TRUE_HIST=efficiency_numerator
+TRUE_HIST=${VARIABLE}_efficiency_numerator
 WARPED_FILE=$2
-RECO_HIST=selected_mc_reco
+RECO_HIST=${VARIABLE}_selected_mc_reco
 
 OUTFILE_NAME=$(basename $2)
 
-TransWarpExtraction --output_file Warping_$OUTFILE_NAME --data $RECO_HIST --data_file $WARPED_FILE --data_truth $TRUE_HIST --data_truth_file $WARPED_FILE --migration migration --migration_file $MIGRATION_FILE --reco $RECO_HIST --reco_file $MIGRATION_FILE --truth $TRUE_HIST --truth_file $MIGRATION_FILE --num_iter 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,40,50,60,70,80,90,100 --num_uni 100
+TransWarpExtraction --output_file Warping_$OUTFILE_NAME --data $RECO_HIST --data_file $WARPED_FILE --data_truth $TRUE_HIST --data_truth_file $WARPED_FILE --migration ${VARIABLE}_migration --migration_file $MIGRATION_FILE --reco $RECO_HIST --reco_file $MIGRATION_FILE --truth $TRUE_HIST --truth_file $MIGRATION_FILE --num_iter 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,40,50,60,70,80,90,100 --num_uni 100
